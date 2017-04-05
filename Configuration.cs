0d29accddc0da4e0c91b43fb46e5b7dbe1aafb59
SDPlugins <admin@sdplugins.com>
2017-04-05 23:54:44 +0100

    Project Created

---
+++ Configuration.cs
@@ -0,0 +1,44 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace PocketResizer
+{
+	public class Configuration : IRocketPluginConfiguration
+	{
+		[XmlArray ("Pockets")]
+		[XmlArrayItem ("Pocket")]
+		public List<pocket_size> pockets;
+
+		public void LoadDefaults ()
+		{
+			pockets = new List<pocket_size> ()
+			{
+				new pocket_size (3, 2, "pocket.small"),
+				new pocket_size (10, 4, "pocket.huge"),
+				new pocket_size (10, 10, "pocket.insane")
+			};
+		}
+	}
+
+	public class pocket_size
+	{
+		[XmlElement ("Width")]
+		public int width;
+		[XmlElement ("Height")]
+		public int height;
+		[XmlElement ("Permission")]
+		public string permission;
+
+		public pocket_size () { width = 5; height = 2; permission = "default"; }
+		public pocket_size (int width, int height, string permission)
+		{
+			this.width = width;
+			this.height = height;
+			this.permission = permission;
+		}
+	}
+}
---
+++ PocketResizer.cs
@@ -0,0 +1,57 @@
+﻿using Rocket.Core;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Enumerations;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PocketResizer
+{
+    public class PocketResizer : RocketPlugin<Configuration>
+    {
+		public static PocketResizer Instance;
+
+		protected override void Load ()
+		{
+			Instance = this;
+			UnturnedPlayerEvents.OnPlayerUpdateGesture += GestureChange;
+		}
+
+		private void GestureChange (UnturnedPlayer player, UnturnedPlayerEvents.PlayerGesture gesture)
+		{
+			if (gesture == UnturnedPlayerEvents.PlayerGesture.InventoryOpen)
+			{
+
+				pocket_size best = getBestSize (player);
+				if (best.permission != "$$$$$DO NOT USE$$$$$")
+				{
+					player.Inventory.items [2].resize ((byte)best.width, (byte)best.height);
+				}
+			}
+		}
+
+		protected override void Unload ()
+		{
+			UnturnedPlayerEvents.OnPlayerUpdateGesture -= GestureChange;	
+		}
+
+		public pocket_size getBestSize (UnturnedPlayer player)
+		{
+			pocket_size ret = new pocket_size (-1, -1, "$$$$$DO NOT USE$$$$$");
+
+			foreach (var p in Configuration.Instance.pockets)
+			{
+				if (R.Permissions.HasPermission (player, new List<string> () { p.permission }))
+					if ((p.width * p.height) > (ret.width * ret.height))
+						ret = p;
+			}
+
+			return ret;
+		}
+	}
+}
